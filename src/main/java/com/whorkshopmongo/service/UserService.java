99fe7db8426aa370dee104ac49c528a0c2403ec5
Rafael Souza <rafael.ssilva131@gmail.com>
2024-01-06 14:40:07 -0300

    delete

--- src/main/java/com/whorkshopmongo/service/UserService.java
+++ src/main/java/com/whorkshopmongo/service/UserService.java
@@ -30,6 +30,10 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
