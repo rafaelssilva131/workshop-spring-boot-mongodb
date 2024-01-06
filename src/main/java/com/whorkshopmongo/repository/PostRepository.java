101e81f27c0fcb011defc3d834309103e602b2fb
Rafael Souza <rafael.ssilva131@gmail.com>
2024-01-06 16:41:40 -0300

    criando post com autor

---
+++ src/main/java/com/whorkshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.whorkshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.whorkshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
