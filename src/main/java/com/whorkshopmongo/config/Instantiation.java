598d32a337ea1bfae8405069356bfc1c7eaf517d
Rafael Souza <rafael.ssilva131@gmail.com>
2024-01-06 02:08:08 -0300

    instanciando a base de dados

---
+++ src/main/java/com/whorkshopmongo/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.whorkshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import com.whorkshopmongo.domain.User;
+import com.whorkshopmongo.repository.UserRepository;
+
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userReposiroty;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userReposiroty.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
