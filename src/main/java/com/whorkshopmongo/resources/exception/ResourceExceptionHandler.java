56593fd7b228dd3aace8f958407e5307b16a2356
Rafael Souza <rafael.ssilva131@gmail.com>
2024-01-06 14:34:18 -0300

    insert

--- src/main/java/com/whorkshopmongo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/whorkshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -1,12 +1,9 @@
 package com.whorkshopmongo.resources.exception;
 
-import javax.net.ssl.SSLEngineResult.Status;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.whorkshopmongo.service.exception.ObjectNotFoundException;
 
