1a69b5ec66ee4edfede261c632d6049f9f13456f
Rafael Souza <rafael.ssilva131@gmail.com>
2024-01-06 18:59:56 -0300

    buscando por varios parametros

--- src/main/java/com/whorkshopmongo/resources/PostResource.java
+++ src/main/java/com/whorkshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.whorkshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +28,23 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@RequestMapping(value="/titlesourch", method=RequestMethod.GET)
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
  	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> fullSuerch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate, 
+			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		
 		return ResponseEntity.ok().body(list);
 		}
