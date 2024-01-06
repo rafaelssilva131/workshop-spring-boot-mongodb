8ac08971de2b2c0d3c6675b1a14fadbac17e71f7
Rafael Souza <rafael.ssilva131@gmail.com>
2024-01-06 14:20:41 -0300

    obtendo user pelo id

---
+++ src/main/java/com/whorkshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.whorkshopmongo.resources.exception;
+
+import javax.net.ssl.SSLEngineResult.Status;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.whorkshopmongo.service.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(),status.value(), "Não encontrado", e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
